64e5a173a71872d457dce5811b511d2d64f7f344
Namit Yadav <yadavnamit605@gmail.com>
2024-06-13 01:27:30 +0800

    build command is finalized

---
+++ Client/webpack.config.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+  entry: './src/main.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
+
+export default config;
