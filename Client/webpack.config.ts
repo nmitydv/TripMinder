93b598e9aa65e6f08e006b1893852fadd2bc50f8
Namit Yadav <yadavnamit605@gmail.com>
2024-06-13 02:28:05 +0800

    create a build

--- Client/webpack.config.ts
+++ Client/webpack.config.ts
@@ -7,22 +7,34 @@ const config: Configuration = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: 'ts-loader',
+        test: /\.tsx?$/,  // Match both .ts and .tsx files
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,  // Match image files
+        type: 'asset/resource',  // or use 'file-loader'
+      },
+      {
+        test: /\.(mp3|wav|ogg)$/i,  // Match audio files
+        type: 'asset/resource',  // or use 'file-loader'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,  // Match font files
+        type: 'asset/resource',  // or use 'file-loader'
+      },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
 
